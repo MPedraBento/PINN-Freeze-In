33eea018b5c5bcc613844bd28c82c1b9232f780c
MPedraBento <miguelfilipebento@gmail.com>
2025-02-24 16:46:29 +0000

    feat(notebooks): uploaded notebooks and plots

---
+++ notebooks/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+G = 1.0
+G_ASTS = 100.0
+
+Z0 = np.log(1.0e-14)
+ZF = np.log(100.0)
+
+W0 = np.log(1.0e-25)
+WF = -26.1586
+
+GAMMA_STD = 0.0
+GAMMA_RS = 2.0
+GAMMA_GB = -2.0/3.0
+
+C_STD = -11.6964
+C_RS = -7.4967
+C_GB = -13.5804
+
+ZT1 = np.log(10.0)
+ZT2 = np.log(1.0)
